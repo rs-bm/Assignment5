b30e4828de409da3ae8a2e3b0cf63a052e79baea
rs-bm <william.swanson@bruins.belmont.edu>
2025-11-25 21:49:55 -0600

    added move, ability, image

--- app/src/main/java/com/example/assignment5/MainActivity.java
+++ app/src/main/java/com/example/assignment5/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.assignment5;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.ANRequest;
@@ -25,7 +29,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
     ListView watchlistLV;
     EditText searchET;
     Button searchB;
+    Button clearB;
     ImageView iv;
+    ConstraintLayout profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +46,17 @@ public class MainActivity extends AppCompatActivity {
         searchET = findViewById(R.id.searchET);
         searchB = findViewById(R.id.searchB);
         searchB.setOnClickListener(searchListener);
+        clearB = findViewById(R.id.clearB);
+        clearB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profile.setVisibility(INVISIBLE);
+                searchET.setText("");
+            }
+        });
         iv = findViewById(R.id.logoView);
+        profile = findViewById(R.id.profileLayout);
+        profile.setVisibility(INVISIBLE);
         watchlistLV = findViewById(R.id.watchlistLV);
         watchlistLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -57,23 +72,31 @@ public class MainActivity extends AppCompatActivity {
         req.getAsJSONObject(new JSONObjectRequestListener() {
             @Override
             public void onResponse(JSONObject jsonObject) {
-                String imageURL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png" + "001" + ".png";
-                ImageView iv = findViewById(R.id.logoView);
-                Picasso.get().load(imageURL).into(iv);
                 try {
-                    // Populate listview on left side
-                    String row = jsonObject.getInt("id") + "   " + jsonObject.getString("name");
+                    // Populate listview on left side (if pokemon is not a duplicate)
+                    int id = jsonObject.getInt("id");
+                    String row = String.format("%-10s", id) + jsonObject.getString("name");
                     if (!watchlist.contains(row)) {
                         watchlist.add(row);
                         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, watchlist);
                         watchlistLV.setAdapter(adapter);
                     }
                     // Fill out fields on right side
-                    ((TextView) findViewById(R.id.pokeNameTV)).setText(jsonObject.getString("name"));
-                    ((TextView) findViewById(R.id.numberField)).setText(String.valueOf(jsonObject.getInt("id")));
+                    ((TextView) findViewById(R.id.pokeNameTV)).setText(jsonObject.getString("name").toUpperCase());
+                    ((TextView) findViewById(R.id.numberField)).setText(String.valueOf(id));
                     ((TextView) findViewById(R.id.weightField)).setText(String.valueOf(jsonObject.getInt("weight")));
                     ((TextView) findViewById(R.id.heightField)).setText(String.valueOf(jsonObject.getInt("height")));
                     ((TextView) findViewById(R.id.baseXPField)).setText(String.valueOf(jsonObject.getInt("base_experience")));
+                    String move = jsonObject.getJSONArray("moves").getJSONObject(0).getJSONObject("move").getString("name");
+                    ((TextView) findViewById(R.id.moveField)).setText(move);
+                    String ab = jsonObject.getJSONArray("abilities").getJSONObject(0).getJSONObject("ability").getString("name");
+                    ((TextView) findViewById(R.id.abilityField)).setText(ab);
+                    // Load image
+                    String imageURL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/" + String.format("%03d", id) + ".png";
+                    ImageView iv = findViewById(R.id.logoView);
+                    Picasso.get().load(imageURL).into(iv);
+                    // Show profile
+                    profile.setVisibility(VISIBLE);
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
@@ -81,17 +104,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(ANError anError) {
+                Toast.makeText(getApplicationContext(), "Pokemon not found", Toast.LENGTH_SHORT).show();
                 Log.i("ERR", req.getUrl());
                 Log.i("ERR", anError.getErrorDetail());
                 Log.i("ERR", anError.getErrorBody());
                 Log.i("ERR", anError.getErrorCode() + "");
                 Log.i("ERR", anError.getResponse().toString());
-                Toast.makeText(getApplicationContext(), "error getting data", Toast.LENGTH_LONG).show();
-                Toast.makeText(getApplicationContext(), anError.getErrorDetail(), Toast.LENGTH_LONG).show();
-                Toast.makeText(getApplicationContext(), req.getUrl(), Toast.LENGTH_LONG).show();
             }
         });
+
     }
+
 
     View.OnClickListener searchListener = new View.OnClickListener() {
         @Override
@@ -100,18 +123,21 @@ public class MainActivity extends AppCompatActivity {
             if (validSearch(s)) {
                 getPkmn(s);
                 searchET.setText("");
-            } else {
-                Toast.makeText(getApplicationContext(), "Invalid search", Toast.LENGTH_SHORT).show();
             }
         }
     };
     private boolean validSearch(String s) {
-        if (s.isBlank() || !s.matches("^[^%&*(@)!;:<>]*$")) {
+        if (s.isBlank()) {
+            return false;
+        }
+        else if (!s.matches("^[^%&*(@)!;:<>]*$")) {
+            Toast.makeText(getApplicationContext(), "Search contains invalid characters", Toast.LENGTH_SHORT).show();
             return false;
         }
         try {
             int i = Integer.parseInt(s);
             if (i < 1 || i > 1010) {
+                Toast.makeText(getApplicationContext(), "ID number out of range", Toast.LENGTH_SHORT).show();
                 return false;
             }
         } catch (NumberFormatException e) {
