f53004ae72edae6fcf917520190e065b81052563
rs-bm <william.swanson@bruins.belmont.edu>
2025-11-25 19:04:43 -0600

    Initial commit - missing image and scrolling

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.assignment5"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.example.assignment5"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.github.amitshekhariitbhu.Fast-Android-Networking:android-networking:1.0.4")
+}
---
+++ app/src/main/java/com/example/assignment5/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.assignment5;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> watchlist;
+    ListView watchlistLV;
+    EditText searchET;
+    Button searchB;
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchET = findViewById(R.id.searchET);
+        searchB = findViewById(R.id.searchB);
+        searchB.setOnClickListener(searchListener);
+        iv = findViewById(R.id.logoView);
+        watchlistLV = findViewById(R.id.watchlistLV);
+        watchlistLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = watchlist.get(position);
+                getPkmn(s.substring(0, s.indexOf(" ")));
+            }
+        });
+        watchlist = new ArrayList<>();
+    }
+    private void getPkmn(String s) {
+        ANRequest req = AndroidNetworking.get("https://pokeapi.co/api/v2/pokemon/" + s + "/").setPriority(Priority.LOW).build();
+        req.getAsJSONObject(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                String imageURL = "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png" + "001" + ".png";
+                ImageView iv = findViewById(R.id.logoView);
+                Picasso.get().load(imageURL).into(iv);
+                try {
+                    // Populate listview on left side
+                    String row = jsonObject.getInt("id") + "   " + jsonObject.getString("name");
+                    if (!watchlist.contains(row)) {
+                        watchlist.add(row);
+                        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, watchlist);
+                        watchlistLV.setAdapter(adapter);
+                    }
+                    // Fill out fields on right side
+                    ((TextView) findViewById(R.id.pokeNameTV)).setText(jsonObject.getString("name"));
+                    ((TextView) findViewById(R.id.numberField)).setText(String.valueOf(jsonObject.getInt("id")));
+                    ((TextView) findViewById(R.id.weightField)).setText(String.valueOf(jsonObject.getInt("weight")));
+                    ((TextView) findViewById(R.id.heightField)).setText(String.valueOf(jsonObject.getInt("height")));
+                    ((TextView) findViewById(R.id.baseXPField)).setText(String.valueOf(jsonObject.getInt("base_experience")));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                Log.i("ERR", req.getUrl());
+                Log.i("ERR", anError.getErrorDetail());
+                Log.i("ERR", anError.getErrorBody());
+                Log.i("ERR", anError.getErrorCode() + "");
+                Log.i("ERR", anError.getResponse().toString());
+                Toast.makeText(getApplicationContext(), "error getting data", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), anError.getErrorDetail(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), req.getUrl(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    View.OnClickListener searchListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String s = searchET.getText().toString();
+            if (validSearch(s)) {
+                getPkmn(s);
+                searchET.setText("");
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid search", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    private boolean validSearch(String s) {
+        if (s.isBlank() || !s.matches("^[^%&*(@)!;:<>]*$")) {
+            return false;
+        }
+        try {
+            int i = Integer.parseInt(s);
+            if (i < 1 || i > 1010) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+        }
+        return true;
+    }
+}
